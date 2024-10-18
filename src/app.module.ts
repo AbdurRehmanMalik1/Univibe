d5c0b31b0c0e5024d8a6481bb70b816a93fc4546
hassan <hassanyousaf941@gmail.com>
2024-10-18 20:42:00 +0500

    added typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',          // Database type
+      host: 'dbprojectfast.database.windows.net',      // Your MS SQL Server host
+      port: 1433,             // Default port for SQL Server
+      username: 'manu', 
+      password: 'abdurrehman0!',
+      database: 'dbProject',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],  // Path to your entities
+      synchronize: true,      // Auto-sync entities with DB (disable in production)
+      extra: {
+        trustServerCertificate: true,  // Required for development environments with self-signed certificates
+      },
+    }),
+  ],
 })
 export class AppModule {}
+
