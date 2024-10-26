970c9ceda0169cf2b6383ce92497867e64071c7b
AbdurRehmanMalik1 <abdurrehman4415@gmail.com>
2024-10-26 18:34:49 +0500

    Using Provider for authentication managmenet

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/signup/contact_info.dart';
 import 'package:frontend/signup/login.dart';
 import 'package:frontend/signup/signup_username.dart';
 import 'package:frontend/signup/verification.dart';
 import './signup/signup.dart'; // Adjust the import based on your file structure
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'storage/authentication.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  LoginPage(), // Set SignUpPage as the home page
+      home:  ContactInfoPage(username: '',), // Set SignUpPage as the home page
     );
   }
 }
